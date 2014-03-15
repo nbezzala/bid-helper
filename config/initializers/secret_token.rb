c3001198ef8d3212b58b7639be739a2a0ecb2d41
Nitish Bezzala <nbezzala@yahoo.com>
2014-03-15 18:20:23 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BidHelper::Application.config.secret_key_base = 'a46f30042ca8a6b30a8ff48958b77d422ec86d6117ee17d236ad56f8c2f400cded1ae9d6472ea669e52f241b1f2fb1f707bc12f7aad2d2cf47664e8c7b7d9dcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BidHelper::Application.config.session_store :cookie_store, key: '_bid-helper_session'
