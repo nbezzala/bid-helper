b7f1fa31dd7202e7e000e045081b39de7c397ab3
Nitish Bezzala <nbezzala@yahoo.com>
2014-03-22 22:31:53 +1300

    Dropped Tasks table and generated new from model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322092403) do
+ActiveRecord::Schema.define(version: 20140322092734) do
 
   create_table "task_seekers", force: true do |t|
     t.datetime "created_at"
