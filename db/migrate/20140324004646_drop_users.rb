96d1f4a8f04350304a78d8aa8b2c3dc4ba92bade
Nitish Bezzala <nbezzala@yahoo.com>
2014-03-24 13:50:20 +1300

    Deleted the users table. Will follow tutorial instructions. Learnt migrations

---
+++ db/migrate/20140324004646_drop_users.rb
@@ -0,0 +1,14 @@
+class DropUsers < ActiveRecord::Migration
+  def up
+  	drop_table :users
+  end
+
+  def down
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+  	end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322092734) do
+ActiveRecord::Schema.define(version: 20140324004646) do
 
   create_table "task_seekers", force: true do |t|
     t.datetime "created_at"
@@ -19,15 +19,6 @@ ActiveRecord::Schema.define(version: 20140322092734) do
   end
 
   create_table "tasks", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.date     "signup_date"
-    t.date     "last_access"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
