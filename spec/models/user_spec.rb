1366e9b10b3fc3e191ecc0d441345a79752fd0de
Nitish Bezzala <nbezzala@yahoo.com>
2014-03-24 14:53:27 +1300

    Validate that name and email are present

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,16 @@ describe User do
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 
+	it { should be_valid }
+
+	describe "When name is not present" do
+		before { @user.name = "" }
+		it { should_not be_valid }
+	end
+
+	describe "When email is not present" do
+		before { @user.email = "" }
+		it { should_not be_valid }
+	end
+	
 end
