dd258af12a29014494444b42a35d453872b6f4a5
Nitish Bezzala <nbezzala@yahoo.com>
2014-03-24 14:04:08 +1300

    Added tests for the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	before { @user = User.new(name: "Test one", email: "test@one.com") }
+
+	subject { @user }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+
 end
