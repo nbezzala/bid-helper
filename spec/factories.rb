3d9cb246c17da3d8b059154f6b6d013ab319158c
Nitish Bezzala <nbezzala@yahoo.com>
2014-03-24 22:38:28 +1300

    Added a title to user show page, and tests for it

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name 'Nitish Bezzala'
+		email 'nbezzala@yahoo.com'
+		password 'foobar'
+		password_confirmation 'foobar'
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,4 +11,13 @@ subject { page }
   	it { should have_content "Sign Up" }
   	it { should have_title(full_title('Sign up'))}
   end
+
+  describe "user profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+
+  	it { should have_content(user.name) }
+  	it { should have_title(user.name) }
+  end
+
 end
